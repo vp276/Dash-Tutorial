b63fbd9090f95982164e9005d244842d32e00718
lucas see <seelcs12@gmail.com>
2023-11-25 17:05:31 -0800

    Adding feature importance

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ import pandas as pd
 import plotly.express as px
 import os
 import numpy as np
+
+#For modeling
 from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 
 #loading Dataset
@@ -16,11 +21,30 @@ total_path = base_path + '\\Data\\' + file_name
 df = pd.read_csv(total_path)
 
 
-#Calculating aggregated dataset
+#Defining function for training ml model
+def train_model(df):
 
+    X, y = df.drop(columns = ['DEATH_EVENT']), df['DEATH_EVENT']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-#Loading training data
+    #Defining parameters for gridsearch
+    parameters = {'max_depth':[2, 4, 6]}
 
+    #Training
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X_train, y_train)
+
+    #Predicting and making confusion matrix
+    y_pred = clf.predict(X_test)
+    cmatrix = confusion_matrix(y_test, y_pred)
+
+    return cmatrix, clf
+
+#Training model
+cmatrix, model = train_model(df)
+X_cols = df.drop(columns = 'DEATH_EVENT')
+
+#Setting theme
 colors = {'background': '#A5D8DD',
         'text': '#7FDBFF'}
 
@@ -74,6 +98,25 @@ dash_app.layout = html.Div(style={'backgroundColor': colors['background']},
                     dcc.Graph(id = 'Age-Plot',
                             figure={})
                 ],style={'width': '49%', 'display': 'inline-block','position': 'fixed'})
+
+
+            ], className = 'row'),
+
+            html.Br(),
+            #Second row of graphs
+            html.Div([
+
+                #First column
+                html.Div(children = [
+                    dcc.Graph(id = 'confusion-matrix',
+                            figure={})
+                    ],style={'width': '49%', 'display': 'inline-block'}),
+
+                #Second column
+                html.Div([
+                    dcc.Graph(id = 'feature-importance',
+                            figure={})
+                ],style={'width': '49%', 'display': 'inline-block'})
 
 
             ], className = 'row'),
@@ -145,12 +188,48 @@ def update_output_div(bp, sex, anaemia):
                             
     
     age_fig = px.scatter(filtered_df,
-                                      x="age", y="platelets", color = "sex", title = "Scatterplot")
+                                      x="age", y="platelets", color = "DEATH_EVENT", title = "Scatterplot")
     
     return factor_fig, age_fig
 
 
+#callback for second row
+@callback(
+    [Output(component_id='confusion-matrix', component_property='figure'),
+    Output(component_id='feature-importance', component_property='figure')],
+    Input('Sex-Filter', 'value')
+)
+def update_model(value):
+
+    #Making copy of df
+    confusion = cmatrix
+    model_copy = model
+    x_copy = X_cols
+
+    #Aggregating confusion dataframe and plotting
+    y_true = confusion[:,0]
+    y_pred = confusion[:,1]
+    agg_confusion = confusion_matrix(y_true, y_pred)
+    #print(y_true)
+    confusion_fig = px.imshow(agg_confusion, 
+                              labels=dict(x="Predicted Value", 
+                                y="True Value", color="Prediction"),
+                                aspect="auto",
+                                text_auto=True,
+                                title = "Confusion Matrix - Predicted vs Actual Values")
+    
+    #Calculating feature imporance
+    importances = model_copy.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in model_copy.estimators_], axis=0)
+    df_importance = pd.DataFrame(list(zip(x_copy, importances, std)), 
+                                 columns = ['Feature Name','Importance', 'Std'])
+    #importances.head
+    feature_fig =  px.bar(df_importance, x='Feature Name', y='Importance',
+                          title = 'Feature Importance')
+
+
+    return confusion_fig, feature_fig
 
 # Run the app
 if __name__ == '__main__':
-    dash_app.run_server(debug=True)
+    dash_app.run_server(debug=False)
